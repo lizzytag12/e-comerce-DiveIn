73913a445a7cbaaae459b342160ce1c6a0fa22c7
Luisa <luisa.h.morales@gmail.com>
2022-07-27 14:20:12 -0300

    scripts for login done

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,41 @@
 /* Selecting the search form element. */
+//capturo el form
 searchForm = document.querySelector('.search-form');
 
 /* Adding an event listener to the search button. When the user clicks on the search button, the search
 form will toggle the class of active. */
+//capturo el boton de busqueda y le doy una clase o la saco al hacer click
 document.querySelector('#search-btn').onclick = () =>{
 
     searchForm.classList.toggle('active')
 }
 
+
+
+
+/* capturo el contenedor del  formulario */
+let loginForm = document.querySelector('.login-form-container');
+
+/* capturo el icono de apertura del login para luego asignarle el evento que me permitira abrir la vista al login */
+document.querySelector('#login-btn').onclick = () => {
+
+    loginForm.classList.toggle('active')
+}
+
+/* capturo el icono de cierre de la vista login  y le asigno un evento que me permitira cerrar la vista*/
+document.querySelector('#close-login-btn').onclick = () => {
+
+    loginForm.classList.remove('active')
+}
+
+
+
+
+
 /* A function that is being called when the user scrolls. */
+
+// al hacer scroll
+
 window.onscroll = () =>{
 
    /* Removing the class of active from the search form. */
@@ -24,7 +51,7 @@ window.onscroll = () =>{
      
 }
 
-
+//al cargar la pagina:
 window.onload= () =>{
 
     if(window.scrollY > 80){
