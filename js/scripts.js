2741949c80c3c976b7d7e806b7337b3131344dde
Luisa <luisa.h.morales@gmail.com>
2022-07-28 19:21:11 -0300

    blogs script done

--- js/scripts.js
+++ js/scripts.js
@@ -167,7 +167,7 @@ window.onload= () =>{
            
          },
          768: {
-           slidesPerView: 2,
+           slidesPerView: 3,
           
          },
          1024: {
@@ -176,3 +176,30 @@ window.onload= () =>{
          },
        },
      });
+
+
+      /*   reviews script  */
+      var swiper = new Swiper(".blogs-slider", {
+        spaceBetween:10,
+        grabCursor:true, 
+        loop:true,
+        centeredSlides:true,
+        autoplay:{
+         delay:5500,
+         disableOnInteraction:false,
+        },
+         breakpoints: {
+           0: {
+             slidesPerView: 1,
+             
+           },
+           768: {
+             slidesPerView: 2,
+            
+           },
+           1024: {
+             slidesPerView: 3,
+            
+           },
+         },
+       });
